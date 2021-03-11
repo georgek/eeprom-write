e27bc46b333013c5c7329d7143d488a147edc672
George Kettleborough <kettleg@gmail.com>
2021-03-11 21:41:27 +0000

    Add more parameters

--- eeprom_write/cli.py
+++ eeprom_write/cli.py
@@ -1,51 +1,84 @@
 import click
 import serial
-import time
-
-DEVICE = "/dev/ttyUSB0"
-BAUD = 57600
 
 
-def error(message: str) -> None:
-    click.secho(message, fg="red", err=True)
+def error(message: str, *args, **kwargs) -> None:
+    click.secho(message, fg="red", err=True, *args, **kwargs)
 
 
-def important(message: str) -> None:
-    click.secho(message, fg="yellow", err=True)
+def important(message: str, *args, **kwargs) -> None:
+    click.secho(message, fg="yellow", err=True, *args, **kwargs)
 
 
-def success(message: str) -> None:
-    click.secho(message, fg="green", err=True)
+def success(message: str, *args, **kwargs) -> None:
+    click.secho(message, fg="green", err=True, *args, **kwargs)
 
 
-def info(message: str) -> None:
-    click.secho(message, fg="blue", err=True)
+def info(message: str, *args, **kwargs) -> None:
+    click.secho(message, fg="blue", err=True, *args, **kwargs)
 
 
-def echo(message: str) -> None:
-    click.secho(message, err=True)
+def echo(message: str, *args, **kwargs) -> None:
+    click.secho(message, err=True, *args, **kwargs)
+
+
+def validate_hex_padding(ctx, params, value):
+    try:
+        padding = bytes.fromhex(value)
+        if len(padding) != 1:
+            raise click.BadParameter("Only one byte can be provided")
+    except ValueError:
+        raise click.BadParameter(
+            "Please give padding in padded hex, e.g. '00'"
+        )
+
+    return padding
 
 
 @click.command()
 @click.argument("data", type=click.File("rb"))
+@click.option(
+    "-d",
+    "--device",
+    type=click.Path(exists=True),
+    default="/dev/ttyUSB0",
+    show_default=True,
+)
+@click.option("-b", "--baud", type=int, default=57600, show_default=True)
+@click.option(
+    "-p",
+    "--pad-byte",
+    type=str,
+    default="00",
+    callback=validate_hex_padding,
+    show_default=True,
+)
 @click.version_option()
-def cli(data):
+def cli(data, device, baud, pad_byte):
     """
     Write to the Arduino EEPROM programmer
     """
     data_bytes = data.read(2048)
+    info(f"Writing {len(data_bytes)} bytes ")
+
+    if len(data_bytes) < 2048:
+        important(f"Padding to 2048 bytes with 0x{pad_byte.hex()}")
+    elif data.read():
+        important("Input truncated!")
+    info("")
+
     data_bytes += bytes(2048 - len(data_bytes))
 
-    with serial.Serial(DEVICE, BAUD, parity="N", timeout=1) as ser:
+    with serial.Serial(device, baud, parity="N", timeout=1) as ser:
         # wait for the Arduino to initialise and echo the start value
         info("Initialising Arduino...")
         INIT_DATA = b"\x4f"
         while True:
+            click.echo(".", nl=False)
             ser.write(INIT_DATA)
             if ser.read(1) == INIT_DATA:
                 break
-        info("Done.")
-        echo("")
+        info("\nDone.\n")
 
         important("Writing EEPROM...")
         with click.progressbar(data_bytes) as bar:
@@ -59,11 +92,11 @@ def cli(data):
 
         important("Verifying contents...")
         with click.progressbar(data_bytes) as bar:
-            for data_byte in bar:
+            for i, data_byte in enumerate(bar):
                 data_byte = data_byte.to_bytes(1, "big")
                 incoming = ser.read(1)
                 if incoming != data_byte:
-                    error(f"Wrong byte (wanted {data_byte}, got {incoming})")
-                    # return 2
+                    error(f"Wrong byte at {i} (expect {data_byte}, got {incoming})")
+                    return 2
 
-    success("Done.")
+    success("\nDone.")
