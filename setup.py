8b036a89ff3b63911f2b285bc83c462581baac7c
George Kettleborough <kettleg@gmail.com>
2021-03-13 16:46:40 +0000

    Add long description

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 
+from os import path
+
 from setuptools import find_packages, setup
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
     name="eepromino",
@@ -8,6 +14,8 @@ setup(
     author_email="kettleg@gmail.com",
     url="https://github.com/georgek/eepromino",
     packages=find_packages(exclude=["tests"]),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     python_requires=">=3.8",
     install_requires=["click>=7", "pyserial>=3", "hexdump"],
     extras_require={"testing": ["pytest"]},
